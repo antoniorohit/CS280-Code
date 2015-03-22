5c4f2d12ed792631f261be2a8f1618bec5817258
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2015-03-22 15:44:57 -0700

    Where we stopped today

--- HW3/code/main.m
+++ HW3/code/main.m
@@ -1,13 +1,5 @@
-pwd()
+clear();
+close();
 
-data_dir = ['../data/house'];
+a = reconstruct_3d('house');
 
-% images
-I1 = imread([data_dir '/' 'house' '1.jpg']);
-I2 = imread([data_dir '/' 'house' '2.jpg']);
-
-imshow(I1);
-figure()
-imshow(I2);
-
-a = reconstruct_3d('house')
