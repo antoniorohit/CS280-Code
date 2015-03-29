db7e7fe6efa04f3a708def143f83bf9cfb0af9f1
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2015-03-29 14:23:28 -0700

    Some cosmetic changes

--- HW3/code/plot_3d.m
+++ HW3/code/plot_3d.m
@@ -7,12 +7,13 @@ Y1 = points(:,2);
 Z1 = points(:,3);
 
 figure();
-% use plot3 command
+% 3D pointss
 plot3(X1, Y1, Z1, 'R.');
 hold on;
 
-plot3(P1(1,4), P1(2,4), P1(3,4), 'Bo');
-plot3(P2(1,4), P2(2,4), P2(3,4), 'Bo');
+% Camera Centres
+plot3(P1(1,4), P1(2,4), P1(3,4), 'B+');
+plot3(P2(1,4), P2(2,4), P2(3,4), 'B+');
 
 end
 
