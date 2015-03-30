d08822d1cb1f035f8d3a641af43fe966f41a628c
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2015-03-30 07:44:57 -0700

    Added statment to show det of R, to check if it is 1

--- HW3/code/find_rotation_translation.m
+++ HW3/code/find_rotation_translation.m
@@ -11,14 +11,21 @@ function [R, t] = find_rotation_translation(E)
 W = [0 -1  0;
      1  0  0;
      0  0  1];
-
+ 
+%% Translation Matrix
 t = cell(2,1); 
 t{1} = +U(:,end);
 t{2} = -t{1};
- 
+
+%% Rotation Matrix
+% Do we have to check whether the determinant of R is 1?
+
 R = cell(2,1);
 R{1} = U*W*V';
+detR1 = det(R{1})
+
 R{2} = U*W'*V';
+detR2 = det(R{2})
 
 end
 
