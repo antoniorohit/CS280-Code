33863379d9ef5f376d33036fa608e3a3a0420c34
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2015-03-30 15:12:40 -0700

    Added code that catches det(R)~=1

--- HW3/code/find_rotation_translation.m
+++ HW3/code/find_rotation_translation.m
@@ -22,10 +22,15 @@ t{2} = -t{1};
 
 R = cell(2,1);
 R{1} = U*W*V';
-detR1 = det(R{1})
+detR1 = (det(R{1}));
 
 R{2} = U*W'*V';
-detR2 = det(R{2})
+detR2 = (det(R{2}));
+
+if (abs(detR1-1) > 0.001) || (abs(detR2-1) > 0.001)
+    fprintf('Error: Determinant of the rotation matrix is R1=%f, R2=%f\n', detR1, detR2);
+    % What to do
+end
 
 end
 
